3a25821224cc90e28187b1f43c618aa396da94b9
shubham Yadav <sy907219@gmail.com>
2024-04-13 13:34:36 +0530

    final changes

--- script/script.js
+++ script/script.js
@@ -5,11 +5,11 @@ const navMenu = document.querySelector('.nav-menu ul');
 // console.log(hamburgerBtnImg);
 
 function openMenu() {
-    // console.log('hiii');
+    //console.log('hiii');
     navMenu.classList.toggle('active');
 
     if(navMenu.classList.contains('active')) {
-        hamburgerBtnImg.innerHTML = `<img src="./Img/close.png" alt="img">`;
+        hamburgerBtnImg.innerHTML = `<img src="./Img/close.png" alt="closeImg">`;
 
     } else {
         hamburgerBtnImg.innerHTML = `<img src="./Img/menu.png" alt="menuImg">`;
