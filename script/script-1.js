1710dd7cffbba8d7ce755b714963c92b2d82a44d
shubham Yadav <sy907219@gmail.com>
2024-04-13 13:50:34 +0530

    new changes

---
+++ script/script-1.js
@@ -0,0 +1,18 @@
+
+const hamburgerBtnImg = document.querySelector('.nav-right-element button');
+
+const navMenu = document.querySelector('.nav-menu ul');
+
+// console.log(hamburgerBtnImg);
+
+function openMenu() {
+    //console.log('hiii');
+    navMenu.classList.toggle('active');
+
+    if(navMenu.classList.contains('active')) {
+        hamburgerBtnImg.innerHTML = `<img src="../Img/close.png" alt="closeImg">`;
+
+    } else {
+        hamburgerBtnImg.innerHTML = `<img src="../Img/menu.png" alt="menuImg">`;
+    }
+}
