3fb4a487fa7d4e01488a0936d4160aceb07605bb
shubham Yadav <sy907219@gmail.com>
2024-05-12 14:09:43 +0530

    New upadate on client  project

---
+++ HTMl/Sanket-project/script/nav-header.js
@@ -0,0 +1,82 @@
+document.getElementById("nav-header").innerHTML = `
+
+<div class="logo">
+  <a href="/index.html">
+    <img src="./Images/practive-tech.png" alt="Images" />
+  </a>
+</div>
+<div class="left-container">
+  <nav class="nav-menu">
+    <ul class="menu">
+      <li><a href="/why-us.html" class="link">Why pro-active</a></li>
+      <li><a href="#" class="link">Product</a></li>
+      <li><a href="#" class="link">Partners</a></li>
+      <li><a href="#" class="link">Services</a></li>
+      <li>
+        <a href="#" class="link-button" onclick="togglePopup()"
+          >Get in touch</a
+        >
+      </li>
+    </ul>
+  </nav>
+  <div class="btn" onclick="togglePopup()">
+    <a href="#">Get in touch</a>
+  </div>
+  <div class="btn-container">
+    <div class="login-page">
+      <a href="#"><img src="./Images/account_icon.svg" alt="Images" /></a>
+    </div>
+  </div>
+  <button onclick="openMenu()">
+    <img src="./Images/menu.png" alt="imges" />
+  </button>
+</div>
+
+<div id="popupOverlay" class="overlay-container">
+  <div class="popup-box">
+    <h2 style="color: green">Contact Us</h2>
+    <form class="form-container">
+      <label class="form-label" for="name"> First Name: </label>
+      <input
+        class="form-input"
+        type="text"
+        placeholder="First Name"
+        id="name"
+        name="name"
+        required
+      />
+      <label class="form-label" for="name"> Last Name: </label>
+      <input
+        class="form-input"
+        type="text"
+        placeholder="Last Name"
+        id="name"
+        name="name"
+        required
+      />
+      <label class="form-label" for="name"> Phone: </label>
+      <input
+        class="form-input"
+        type="tel"
+        placeholder="Phone"
+        id="name"
+        name="name"
+        required
+      />
+      <label class="form-label" for="email">Email:</label>
+      <input
+        class="form-input"
+        type="email"
+        placeholder="Enter Your Email"
+        id="email"
+        name="email"
+        required
+      />
+
+      <button class="btn-submit" type="submit">Submit</button>
+    </form>
+    <button class="btn-close-popup" onclick="togglePopup()">Close</button>
+  </div>
+</div>
+
+`;
